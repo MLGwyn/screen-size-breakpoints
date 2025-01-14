5f5f0b0a83a4ecedffcb8d21e9de217ad3727d92
Melissa Gwyn <melissagwyn@gmail.com>
2025-01-14 10:47:13 -0500

    added breakpoints, completed exercise.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="bg-purple-800 text-5xl flex justify-center h-screen items-center">
+    <div className="bg-purple-800 font-doto sm:bg-blue-500 sm:font-faculty md:bg-rose-600 md:font-playwrite lg:bg-emerald-950 lg:font-arsenal text-5xl flex justify-center h-screen items-center">
       Hello!
     </div>
   );
